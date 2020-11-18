a1c15baa0a58e743d578da9b2c6b62504665468c
Ben Tsang <ben.tsanghoilun@gmail.com>
2020-11-18 21:15:28 +0000

    cc

--- src/CaseDetails/CaseDetails.js
+++ src/CaseDetails/CaseDetails.js
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
-import Flip from 'react-reveal/Flip';
-import StackGrid from "react-stack-grid";
 import GalleryImage from './GalleryImage';
 
 import './CaseDetails.css';
 
 const CaseDetails = props => {
     const [isLoaded, setisLoaded] = useState(false);
+    const [onChange, setonChange] = useState(false);
     const [data, setdata] = useState();
     const id = useParams().id;
     console.log(id);
@@ -22,9 +21,10 @@ const CaseDetails = props => {
             console.log(responseData);
             
             setisLoaded(true);
+            setonChange(false);
         }
         getCaseDetails();
-    }, []);
+    }, [onChange]);
 
     return(
         <section className='caseDetails' >
@@ -46,7 +46,7 @@ const CaseDetails = props => {
                             <div className='caseNav'>
                                 {
                                     data.key-1 >= 0 &&
-                                    <Link to={'/cases/case'+(data.key-1)}>
+                                    <Link to={'/cases/case'+(data.key-1)} onClick={() => setonChange(true)}>
                                         <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <rect x="0.452271" y="8.60371" width="11.5279" height="1.21346" transform="rotate(-45 0.452271 8.60371)" fill="black"/>
                                             <rect x="1.3103" y="7.78842" width="11.5279" height="1.21346" transform="rotate(45 1.3103 7.78842)" fill="black"/>
@@ -55,7 +55,7 @@ const CaseDetails = props => {
                                 }
                                 {
                                     data.key+1 < 30 &&
-                                    <Link to={'/cases/case'+(data.key+1)}>
+                                    <Link to={'/cases/case'+(data.key+1)} onClick={() => setonChange(true)}>
                                         <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <rect x="10" y="8.64647" width="11.5279" height="1.21346" transform="rotate(135 10 8.64647)" fill="black"/>
                                             <rect x="9.14197" y="9.46176" width="11.5279" height="1.21346" transform="rotate(-135 9.14197 9.46176)" fill="black"/>
